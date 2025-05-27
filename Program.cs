30692ee0898c46d17e3589738d03c1f791d26ba8
franCordobaDeveloper <franCordobaDeveloper>
2025-05-27 14:42:04 -0300

    Ejercicio 1 completo

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+    Ejercicio 1. Construir un programa que permita invertir un número. Verifique que el
+    texto ingresado es de hecho un número y, en caso afirmativo, realice la inversión del
+    número sólo si éste es mayor a 0.
+*/
+
+// int num;
+// int numAInvertir = 0;
+// bool control = false;
+
+// string? numString;
+
+// while (!control)
+// {
+//     Console.WriteLine("Ingrese un numero para invertir");
+//     numString = Console.ReadLine();
+//     control = int.TryParse(numString, out num);
+
+//     if (!control)
+//     {
+//         Console.WriteLine("El valor ingresado es incorrecto: " + numString);
+//     }
+//     else
+//     {
+//         if (num > 0)
+//         {
+//             numAInvertir = 0;
+//             while (num > 0)
+//             {
+//                 numAInvertir = (numAInvertir * 10) + (num % 10);
+//                 num /= 10;
+//             }
+//             Console.WriteLine("El numero invertido : " + numAInvertir);
+//             break;
+//         }
+//         else
+//         {
+//             Console.WriteLine("El numero que ingreso es 0 un negativo");
+//         }
+//     }
+
+
+// }
+
+
+
+
